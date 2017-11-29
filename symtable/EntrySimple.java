6190db791cdb1b3492b21fbcedcdc8efc1b30cd6
Jan Pierry <janpierrycoelho@gmail.com>
2017-11-29 13:18:43 -0200

    algumas duvidas

--- symtable/EntrySimple.java
+++ symtable/EntrySimple.java
@@ -1,7 +1,7 @@
 package symtable;
 
-
-// entrada utilizada para declarar os tipos básicos da linguagem
+//duvida: o que fazer aqui?
+// entrada utilizada para declarar os tipos basicos da linguagem
 public class EntrySimple extends EntryTable {
     public EntrySimple(String n) {
         name = n;
